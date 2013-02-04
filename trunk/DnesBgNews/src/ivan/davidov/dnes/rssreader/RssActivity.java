08d96cc1136cf2da3a7cb1ed51fbd2242f88d676
davidov.i@gmail.com <davidov.i@gmail.com@27ae43a5-baac-89e1-bc54-78052b92f230>
2013-02-04 19:33:27 +0000

    Initial commit for the currently created RSS readers.

---
+++ trunk/DnesBgNews/src/ivan/davidov/dnes/rssreader/RssActivity.java
@@ -0,0 +1,44 @@
+package ivan.davidov.dnes.rssreader;
+
+import ivan.davidov.dnes.rssreader.reader.RssReader;
+import ivan.davidov.dnes.rssreader.widget.RssWidget;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.appwidget.AppWidgetManager;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class RssActivity extends ListActivity {
+
+	private RssListAdapter adapter;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		List<JSONObject> jobs = new ArrayList<JSONObject>();
+		
+		try {
+			jobs = RssReader.getLatestRssFeed(this);			
+		} catch (Exception e) {
+			Log.e("RSS ERROR", "Error loading RSS Feed Stream >> " + e.getMessage() + " //" + e.toString());
+		}
+
+		adapter = new RssListAdapter(this, jobs);
+
+		setListAdapter(adapter);
+		
+		setTitle(R.string.app_name);
+		
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+		
+		RssWidget.updateWidget(this, appWidgetManager, jobs);
+	}
+}
