c13c5371f89fa5c4b698c2c90490c9b4285ff6af
davidov.i@gmail.com <davidov.i@gmail.com@27ae43a5-baac-89e1-bc54-78052b92f230>
2013-02-05 21:41:40 +0000

    Added another rebranded project for www.Gol.bg

---
+++ GolBgNews/src/ivan/davidov/gol/rssreader/RssActivity.java
@@ -0,0 +1,46 @@
+package ivan.davidov.gol.rssreader;
+
+import ivan.davidov.gol.rssreader.reader.RssReader;
+import ivan.davidov.gol.rssreader.util.CommonStringsHelper;
+import ivan.davidov.gol.rssreader.widget.RssWidget;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.appwidget.AppWidgetManager;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class RssActivity extends ListActivity {
+
+	private RssListAdapter adapter;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		List<JSONObject> jobs = new ArrayList<JSONObject>();
+		
+		try {
+			CommonStringsHelper res = new CommonStringsHelper(this.getResources());
+			jobs = RssReader.getLatestRssFeed(res);			
+		} catch (Exception e) {
+			Log.e("RSS ERROR", "Error loading RSS Feed Stream >> " + e.getMessage() + " //" + e.toString());
+		}
+
+		adapter = new RssListAdapter(this, jobs);
+
+		setListAdapter(adapter);
+		
+		setTitle(R.string.app_name);
+		
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+		
+		RssWidget.updateWidget(this, appWidgetManager, jobs);
+	}
+}
