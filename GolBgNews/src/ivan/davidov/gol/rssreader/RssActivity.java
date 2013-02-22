7e69f117c9630de7742f726eaa481c1e5f58e4c7
davidov.i@gmail.com <davidov.i@gmail.com@27ae43a5-baac-89e1-bc54-78052b92f230>
2013-02-22 18:48:18 +0000

    Major update on the data loading & processing - this allows better UI handling.

--- GolBgNews/src/ivan/davidov/gol/rssreader/RssActivity.java
+++ GolBgNews/src/ivan/davidov/gol/rssreader/RssActivity.java
@@ -11,36 +11,64 @@ import org.json.JSONObject;
 
 import android.app.ListActivity;
 import android.appwidget.AppWidgetManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
 
-public class RssActivity extends ListActivity {
-
-	private RssListAdapter adapter;
-
+public class RssActivity extends ListActivity
+{
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		
-		List<JSONObject> jobs = new ArrayList<JSONObject>();
-		
-		try {
-			CommonStringsHelper res = new CommonStringsHelper(this.getResources());
-			jobs = RssReader.getLatestRssFeed(res);			
-		} catch (Exception e) {
-			Log.e("RSS ERROR", "Error loading RSS Feed Stream >> " + e.getMessage() + " //" + e.toString());
-		}
+		AsyncTask<String, Integer, List<JSONObject>> asyncTask = new AsyncTask<String, Integer, List<JSONObject>>()
+		{
+			@Override
+			protected void onPreExecute()
+			{
+				RssActivity.this.setTitle(R.string.default_text);
+			};
+			
+			@Override
+			protected void onPostExecute(List<JSONObject> jobs)
+			{
+				RssActivity.this.setTitle(R.string.loaded_text);
+			};
+			
+			@Override
+			protected List<JSONObject> doInBackground(String... arg0)
+			{
+				List<JSONObject> jobs = new ArrayList<JSONObject>();
+				
+				try {
+					CommonStringsHelper res = new CommonStringsHelper(RssActivity.this.getResources());
+					jobs = RssReader.getLatestRssFeed(res);			
+				} catch (Exception e) {
+					Log.e("RSS ERROR", "Error loading RSS Feed Stream >> " + e.getMessage() + " //" + e.toString());
+				}
 
-		adapter = new RssListAdapter(this, jobs);
+				final RssListAdapter adapter = new RssListAdapter(RssActivity.this, jobs);
 
-		setListAdapter(adapter);
+				RssActivity.this.runOnUiThread(new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						RssActivity.this.setListAdapter(adapter);
+					}
+				});
+				
+				AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(RssActivity.this);
+				
+				RssWidget.updateWidget(RssActivity.this, appWidgetManager, jobs);
+				
+				return jobs;
+			}
+		};
 		
-		setTitle(R.string.loaded_text);
-		
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-		
-		RssWidget.updateWidget(this, appWidgetManager, jobs);
-	}
+		asyncTask.execute("");
+	}	
 }
